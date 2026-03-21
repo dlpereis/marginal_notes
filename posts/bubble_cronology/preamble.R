cca89b5c5e23cd4d76d6d4186c5cfeb3bca3573c
dlpereis <estevesdosreis@hotmail.com>
2026-03-21 14:37:57 -0300

    add: bubble_cronology post

---
+++ posts/bubble_cronology/preamble.R
@@ -0,0 +1,19 @@
+
+library(readr)
+library(readxl)
+
+library(fredr)
+
+library(plotly)
+library(ggplot2)
+
+library(knitr)
+library(kableExtra)
+
+library(urca)
+library(seasonal)
+library(exuber)
+library(MultipleBubbles)
+# library(psymonitor)
+
+library(tidyverse)
